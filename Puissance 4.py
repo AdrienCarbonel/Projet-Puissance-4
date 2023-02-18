3ea67dba33b77ca3ebc37f7ed219782b109e9e43
Adrien <adrien.carbonel@ens.uvsq.fr>
2023-02-18 14:10:01 +0100

    Grille Terminée

--- Puissance 4.py
+++ Puissance 4.py
@@ -1,8 +1,18 @@
 import tkinter as tk
 
+""" Partie Graphique du Puissance 4 """
+
+
+grid_height, grid_width = 6, 7
+
+taille_case_height, taille_case_width = 1080/grid_height, 1920/grid_width
+
+plateau = [[0]*grid_width for j in range(grid_height)]
+size_modif = 2
+shift_x = 50
+shift_y = 30
+
 # Différentes fonctions
-
-
 def afficher_cercle_jaune(event):
         """Fonction permettant d'afficher un cercle jaune"""
         x, y = event.x, event.y
@@ -13,19 +23,44 @@ def afficher_cercle_rouge(event):
     """Fonction permettant d'afficher un cercle rouge"""
     x, y = event.x, event.y
     canevas.create_oval(x,y,(x+50),(y+50),fill="red",width=2, outline="red")
-# Création de la fenêtre
 
+
+def popupmsg():
+    popup = tk.Tk()
+    
+
+   
+
+
+
+
+# Création de la fenêtre
 fenetre = tk.Tk()  # Création de la fenêtre 
 fenetre.title("Un jeu de Puissance 4")  #Titre
 fenetre.geometry("1920x1080") # Dimension de la fenêtre
-# Canevas
 
+
+
+
+# Canevas 
 canevas = tk.Canvas(bg="white",width=1920,height=1080)
-# Placement des widgets
 
+# Création de la Grille
+x = 0
+y = 0
+for i in range(max(grid_height,grid_width)+1):
+    canevas.create_line(x/size_modif+shift_x,shift_y,x/size_modif+shift_x,1080/size_modif+shift_y,fill="blue",width=2)
+    canevas.create_line(shift_x,y/size_modif+shift_y,1920/size_modif+shift_x,y/size_modif+shift_y,fill="blue",width=2)
+    x += taille_case_width
+    y += taille_case_height
+
+# Placement des widgets
 canevas.grid()
 canevas.bind('<Button-3>', afficher_cercle_rouge)
 canevas.bind('<Button-1>', afficher_cercle_jaune)
+
+""" Partie Logique du Puissance 4 """
+
 
 
 
